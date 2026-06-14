cc660a5f6a7611dec9e96fa21ceb9e7c3910bf3e
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-06-14 19:58:43 -0300

    MainView design

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module org.study.worksjavafx {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens org.study.worksjavafx to javafx.fxml;
     exports org.study.worksjavafx;
 }
--- src/main/java/org/study/worksjavafx/Main.java
+++ src/main/java/org/study/worksjavafx/Main.java
@@ -1,4 +1,34 @@
 package org.study.worksjavafx;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+           FXMLLoader loader=new FXMLLoader(getClass().getResource("/org/study/worksjavafx/MainView.fxml"));
+           ScrollPane scrollPane=loader.load();
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+           Scene mainScene=new Scene(scrollPane);
+           primaryStage.setScene(mainScene);
+           primaryStage.setTitle("Sample JavaFX application");
+           primaryStage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
