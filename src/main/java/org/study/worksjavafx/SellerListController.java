8dd56de2ed51866a067fc4fafe648c28e4e3b882
Nicholas Bernardo <nicholasbgraciano@gmail.com>
2026-06-17 20:51:08 -0300

    Seller TableView

--- src/main/java/org/study/worksjavafx/SellerListController.java
+++ src/main/java/org/study/worksjavafx/SellerListController.java
@@ -22,6 +22,7 @@ import org.study.worksjavafx.util.Utils;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -38,6 +39,15 @@ public class SellerListController implements Initializable, DataChangeListener {
 
     @FXML
     private TableColumn<Seller,String> tableColumnName;
+
+    @FXML
+    private TableColumn<Seller,String> tableColumnEmail;
+
+    @FXML
+    private TableColumn<Seller, Date> tableColumnBirthDate;
+
+    @FXML
+    private TableColumn<Seller,Double> tableColumnBaseSalary;
 
     @FXML
     private TableColumn<Seller,Seller> tableColumnREMOVE;
@@ -73,6 +83,11 @@ public class SellerListController implements Initializable, DataChangeListener {
     private void initializeNodes() {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+        Utils.formatTableColumnDate(tableColumnBirthDate,"dd/MM/yyyy");
+        tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+       Utils.formatTableColumnDouble(tableColumnBaseSalary,2);
 
         Stage stage= (Stage)Main.getMainScene().getWindow();
         tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
@@ -87,6 +102,7 @@ public class SellerListController implements Initializable, DataChangeListener {
         tableViewSeller.setItems(obsList);
         initEditButtons();
         initRemoveButtons();
+
     }
 
     private void createDialogForm(Seller obj,String absoluteName,Stage parentStage){
